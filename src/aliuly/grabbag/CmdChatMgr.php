d952ec4e97259e91154d979ef3ed9af4011d8adf
Lyco Tatierra <lycol50@gmail.com>
2021-02-11 22:18:59 +0800

    remove nasty nickname
    remove /nick due to default players can access it

--- src/aliuly/grabbag/CmdChatMgr.php
+++ src/aliuly/grabbag/CmdChatMgr.php
@@ -12,9 +12,6 @@
 //: Clears your chat window
 //> usage: **clearchat**
 //:
-//= cmd:nick,Player_Management
-//: Change your display name
-//> usage: **nick** _<name>_
 
 namespace aliuly\grabbag;
 
@@ -55,10 +52,6 @@ class CmdChatMgr extends BasicCli implements Listener, CommandExecutor{
 			["description" => mc::_("stops chat"),
 				"usage" => mc::_("/chat-off [player|-l|-g]"),
 				"permission" => "gb.cmd.togglechat"]);
-		$this->enableCmd("nick",
-			["description" => mc::_("change displayed name"),
-				"usage" => mc::_("/nick <new-name>"),
-				"permission" => "gb.cmd.nick"]);
 
 		$this->owner->getServer()->getPluginManager()->registerEvents($this, $this->owner);
 	}
@@ -81,16 +74,6 @@ class CmdChatMgr extends BasicCli implements Listener, CommandExecutor{
 
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
 		switch($cmd->getName()){
-			case "nick":
-				if(!MPMU::inGame($sender)) return true;
-				if(count($args) == 0){
-					$sender->sendMessage(mc::_("Current nick is: %1%", $sender->getDisplayName()));
-					return true;
-				}
-				if(count($args) !== 1) return false;
-				$this->owner->getServer(mc::_("%1% is now known as %2%", $sender->getDisplayName(), $args[0]));
-				$sender->setDisplayName($args[0]);
-				return true;
 			case "clearchat":
 				if(!MPMU::inGame($sender)) return true;
 				if(count($args) != 0) return false;
